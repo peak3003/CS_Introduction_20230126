9a2e9083f7f15d7d1b4da26699841e2b19c7eb9a
peak3003 <peak3003@email.com>
2023-01-28 11:03:30 +0300

    Lesson1 03

---
+++ 003/Program.cs
@@ -0,0 +1,15 @@
+﻿// С клавиатуры вводятся два числа. Найти и вывести на экран максимальное из них
+int a, b, max;
+Console.Write("Введите а:");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b:");
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+  max = a;
+}
+else
+{
+  max = b;
+}
+Console.WriteLine($"max={max}");
---
+++ 005/Program.cs
@@ -0,0 +1,18 @@
+﻿// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+int a, b, c, max;
+Console.Write("Введите а:");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b:");
+b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите c:");
+c = Convert.ToInt32(Console.ReadLine());
+max = a;
+if (max < b)
+{
+  max = b;
+}
+if (max < c)
+{
+  max = c;
+}
+Console.WriteLine($"max={max}");
