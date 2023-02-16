57f27643722074fbc5fcb2f8f0b675d838412388
peak3003 <peak3003@email.com>
2023-02-16 22:28:20 +0300

    Lesson 07 1

---
+++ 078/Program.cs
@@ -0,0 +1,16 @@
+﻿//78. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1. Использовать рекурсию.
+
+int Fib(int N)
+{
+    if (N == 0) return 0;
+        else
+            if (N == 1) return 1;
+                else
+                    return Fib(N - 1) + Fib(N - 2);
+}
+
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+//for (i=-10;i!=10;i++)
+    System.Console.WriteLine(Fib(n));
