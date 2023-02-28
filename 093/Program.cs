1101497ae48c7de7d5b160822784ccb7bb50f87e
peak3003 <peak3003@email.com>
2023-02-28 09:39:37 +0300

    Lesson 09

---
+++ 093/Program.cs
@@ -0,0 +1,61 @@
+﻿// 093 Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// а) простым поэлементным перемножением
+
+
+int[,] RandomMatrix(int N, int M, int min, int max)
+{
+    int[,] a = new int[N, M];
+    Random random = new Random();
+    for (int i = 0; i < a.GetLength(0); i++)
+        for (int j = 0; j < a.GetLength(1); j++)
+            a[i, j] = random.Next(min, max + 1);
+    return a;
+}
+
+
+void PrintMatrix(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+            System.Console.Write($"{a[i, j],5}");
+        System.Console.WriteLine();
+    }
+}
+
+int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+{
+    if (matrixA.GetLength(1) != matrixB.GetLength(0))
+    {
+        throw new Exception("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+    }
+    var matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+    for (var i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (var j = 0; j < matrixB.GetLength(1); j++)
+        {
+            matrixC[i, j] = matrixA[i, j]*matrixB[i, j];
+        
+        }
+    }
+    return matrixC;
+}
+Console.Write("Введите кол-во строк массива M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов массива N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение элементов массива: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int[,] a = RandomMatrix(m, n, 0, z);
+int[,] b = RandomMatrix(m, n, 0, z);
+System.Console.WriteLine("Матрица A:");
+PrintMatrix(a);
+System.Console.WriteLine("Матрица B:");
+PrintMatrix(b);
+//int[,] c = new[,];
+var c = MatrixMultiplication(a, b);
+System.Console.WriteLine("Получившаяся матрица C:");
+PrintMatrix(c);
+
---
+++ 098/Program.cs
@@ -0,0 +1,38 @@
+﻿// 98. Показать треугольник Паскаля
+Console.Write("Введите количество строк : ");
+int row = Convert.ToInt32(Console.ReadLine());
+int [,] triangle=new int [row,row];
+
+
+void FillTriangle()
+{
+    for (int i=0;i<row;i++)
+    {
+        triangle[i,0]=1;
+        triangle[i,i]=1;
+    }
+    for (int i=2;i<row;i++)
+    {
+        for (int j=1;j<=i;j++)
+        {
+            triangle[i,j]=triangle[i-1,j-1]+triangle[i-1,j];
+        }
+    }
+
+}
+
+void PrintTriangle()
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<row;j++)
+        {
+            if (triangle[i,j]!=0)
+            System.Console.Write($"{triangle[i,j],5}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+FillTriangle();
+PrintTriangle();
